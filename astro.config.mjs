b38f20d3d0c2863b7d9a45490155f9d3400954c7
Igro-Zona <tim.bic22@gmail.com>
2025-08-18 20:22:31 +0300

    stat of configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://iznanka.netlify.app/",
-  integrations: [preact()],
+	site: "https://iznanka.netlify.app/",
+	base: "/",
+	integrations: [sitemap()],
 });
